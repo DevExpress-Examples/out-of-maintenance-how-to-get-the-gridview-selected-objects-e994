67c8596660133022f08c774d1ca2bbf85621c6e2
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to get the GridView selected objects
+
+
+<p>To get all selected objects in the GridView, you should iterate via all selected rows, get row handles, and then by using the GetSelectedRows method get the selected row object itself.<br />
+<strong>See Also:</strong><br />
+<a href="http://documentation.devexpress.com/#WindowsForms/CustomDocument711">Selections Overview</a></p>
+
+<br/>
+
+
---
+++ VB/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,95 @@
+Imports Microsoft.VisualBasic
+Imports System
+Namespace WindowsApplication1
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+			Me.simpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+			Me.listBoxControl1 = New DevExpress.XtraEditors.ListBoxControl()
+			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.listBoxControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' gridControl1
+			' 
+			Me.gridControl1.EmbeddedNavigator.Name = ""
+			Me.gridControl1.Location = New System.Drawing.Point(12, 41)
+			Me.gridControl1.MainView = Me.gridView1
+			Me.gridControl1.Name = "gridControl1"
+			Me.gridControl1.Size = New System.Drawing.Size(384, 315)
+			Me.gridControl1.TabIndex = 0
+			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
+			' 
+			' gridView1
+			' 
+			Me.gridView1.GridControl = Me.gridControl1
+			Me.gridView1.Name = "gridView1"
+			Me.gridView1.OptionsSelection.MultiSelect = True
+			Me.gridView1.OptionsView.NewItemRowPosition = DevExpress.XtraGrid.Views.Grid.NewItemRowPosition.Bottom
+			' 
+			' simpleButton1
+			' 
+			Me.simpleButton1.Location = New System.Drawing.Point(12, 12)
+			Me.simpleButton1.Name = "simpleButton1"
+			Me.simpleButton1.Size = New System.Drawing.Size(118, 23)
+			Me.simpleButton1.TabIndex = 1
+			Me.simpleButton1.Text = "GetSelectedObjects"
+'			Me.simpleButton1.Click += New System.EventHandler(Me.simpleButton1_Click);
+			' 
+			' listBoxControl1
+			' 
+			Me.listBoxControl1.Location = New System.Drawing.Point(402, 41)
+			Me.listBoxControl1.Name = "listBoxControl1"
+			Me.listBoxControl1.Size = New System.Drawing.Size(145, 315)
+			Me.listBoxControl1.TabIndex = 3
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(559, 368)
+			Me.Controls.Add(Me.listBoxControl1)
+			Me.Controls.Add(Me.simpleButton1)
+			Me.Controls.Add(Me.gridControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.listBoxControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private gridControl1 As DevExpress.XtraGrid.GridControl
+		Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+		Private WithEvents simpleButton1 As DevExpress.XtraEditors.SimpleButton
+		Private listBoxControl1 As DevExpress.XtraEditors.ListBoxControl
+	End Class
+End Namespace
+
